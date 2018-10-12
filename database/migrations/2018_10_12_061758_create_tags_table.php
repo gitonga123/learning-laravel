7eafe8881626bb78dc643c0e0c04e66e0684b7af
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-12 09:23:35 +0300

    migrate tags

---
+++ database/migrations/2018_10_12_061758_create_tags_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('book_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('book_tag');
+    }
+}
