39d6544a6b2ba8c068e82ceba61cdb220ca1a792
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-12 09:07:03 +0300

    Migrations

---
+++ database/migrations/2018_10_12_053941_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 200)->unique()->index();
+            $table->string('isbn');
+            $table->integer('page_count');
+            $table->decimal('price', 5, 2);
+            $table->text('description');
+            $table->integer('author_id')->unsigned();
+            $table->integer('publisher_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
