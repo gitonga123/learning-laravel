e1d6bfaea9dc1ddba9e313c825ee47e5639b8ee7
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-14 08:59:24 +0300

    Using Softdeletes

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    //a local scope
 }
