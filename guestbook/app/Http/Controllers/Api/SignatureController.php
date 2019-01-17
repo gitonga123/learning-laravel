cc47fb0734b3d005702a4d8b7a8f47798ea91ec6
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-17 14:47:30 +0300

    Resources, Transformers, Testing Endpoints :coffee:

---
+++ guestbook/app/Http/Controllers/Api/SignatureController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Signature;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SignatureResource;
+
+class SignatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $signatures = Signature::latest()
+            ->ignoreFlagged()
+            ->paginate(20);
+
+        return SignatureResource::collection($signatures);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return SignatureResource
+     */
+    public function store(Request $request)
+    {
+        $signature = $this->validate(
+            $request,
+            [
+                'name' => 'required|min:3|max:50',
+                'email' => 'required|email',
+                'body' => 'required|min:3'
+            ]
+        );
+
+        $signature = Signature::create($signature);
+
+        return new SignatureResource($signature);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Signature  $signature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Signature $signature)
+    {
+        return new SignatureResource($signature);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Signature  $signature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Signature $signature)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Signature  $signature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Signature $signature)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Signature  $signature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Signature $signature)
+    {
+        //
+    }
+}
--- guestbook/routes/web.php
+++ guestbook/routes/web.php
@@ -9,8 +9,13 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('signatures', 'Api\SignatureController')
+    ->only(['index', 'store', 'show']);
+
+Route::put('signatures/{signatures}/report', 'Api\ReportSignature@update');
