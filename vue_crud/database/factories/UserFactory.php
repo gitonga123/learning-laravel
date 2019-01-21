76a0f3357bb4e286b550da1ed8a5f51b57fcdd62
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-21 12:27:40 +0300

    Posts Factory :simple:

--- vue_crud/database/factories/UserFactory.php
+++ vue_crud/database/factories/UserFactory.php
@@ -11,15 +11,29 @@ use Faker\Generator as Faker;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
-*/
+ */
 
-$factory->define(App\User::class, function (Faker $faker) {
-    static $password;
+$factory->define(
+    App\User::class,
+    function (Faker $faker) {
+        static $password;
 
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
+        return [
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'password' => $password ? : $password = bcrypt('secret'),
+            'remember_token' => str_random(10),
+        ];
+    }
+);
+
+
+$factory->define(
+    App\Post::class,
+    function (Faker $faker) {;
+        return [
+            'title' => $faker->sentence(),
+            'body' => $faker->paragraph()
+        ];
+    }
+);
